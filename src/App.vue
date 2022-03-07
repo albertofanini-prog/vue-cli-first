865ab1fc66fe4fc250f72ad91b1b45f677939238
albertofanini-prog <albertofanini@outlook.com>
2022-03-07 17:18:46 +0100

    Commit

--- src/App.vue
+++ src/App.vue
@@ -21,12 +21,15 @@ export default {
 </script>
 
 <style lang="scss">
+body{
+  background-color: #2c3e50;
+  color: white;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 #mrBean{
